554a2347e8ae154a3dcfdc87cd840ae2374dabc9
amir-suhail <aamirs98@gmail.com>
2024-02-15 11:45:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 run();
-function run() {
-  alert('in');
+function run()
+{
+  let playerList = getPlayList();
+  console.log('playerList : ', playerList)
+}
+
+function getPlayList()
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", "https://sheets.googleapis.com/v4/spreadsheets/13KfhY0zVeFHNXjdKgSgYSYYnvVc7sh5x8m4Gbi4bTk4/values/Sheet1?key=AIzaSyD7XwZblV4qvWVH6sKh0F7Iry56MbVZWmo", false );
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
 }
